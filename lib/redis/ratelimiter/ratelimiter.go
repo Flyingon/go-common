4c465b2bbd889d6838ceef3c1a95c986787a145e
joeeyuan <joeeyuan@tencent.com>
2021-03-30 21:26:37 +0800

    UPDATE

--- lib/redis/ratelimiter/ratelimiter.go
+++ lib/redis/ratelimiter/ratelimiter.go
@@ -23,26 +23,30 @@ func (r *RateLimiter) CheckRate(redisKey, limitKey string) error {
 		return nil
 	}
 	key := fmt.Sprintf("%s:%s", redisKeyPrefix, limitKey)
-	rs, err := redis.GetRedisClt(redisKey).Get(key)
+	count, err := redis.GetRedisClt(redisKey).IncrBy(key, 1)
 	if err != nil {
 		util.ReportMonitor(fmt.Sprintf("限频key(%s)查询失败", limitKey), 1, 0)
 		err = fmt.Errorf("ratelimit key(%s) get failed, err: %v", limitKey, err)
 		log.Error(err.Error())
 		return nil // 这里可能是限频的redis出问题了，降级为保证业务请求
 	}
-	curNum := util.InterfaceToInt(rs)
-	if curNum > r.LimitNum {
+	if count == 1 {
+		err = redis.GetRedisClt(redisKey).Expire(key, r.LimitInterval)
+		if err != nil {
+			util.ReportMonitor(fmt.Sprintf("%s设置限频失败", limitKey), 1, 0)
+			return err
+		}
+	}
+	if count > r.LimitNum {
+		if ttlRes, e := redis.GetRedisClt(redisKey).Ttl(key); e == nil {
+			if ttlRes == -1 {
+				redis.GetRedisClt(redisKey).Del([]string{key})
+			}
+		} else {
+			util.ReportMonitor(fmt.Sprintf("%s限频有效期检查失败", limitKey), 1, 0)
+		}
 		util.ReportMonitor(fmt.Sprintf("%s触发限频", limitKey), 1, 0)
 		err = fmt.Errorf("%s rate is exceed limit", limitKey)
-		return err
-	}
-	var count int
-	count, err = redis.GetRedisClt(redisKey).IncrBy(key, 1)
-	if count == 1 && err == nil {
-		err = redis.GetRedisClt(redisKey).Expire(key, r.LimitInterval)
-	}
-	if err != nil {
-		util.ReportMonitor(fmt.Sprintf("%s设置限频失败", limitKey), 1, 0)
 		return err
 	}
 	return nil
